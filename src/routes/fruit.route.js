2d3bf1ec811dc53373edd2b88a9b1ffb52d88605
moses-20 <mosesgameli20@gmail.com>
2021-11-17 20:22:41 +0000

    dev: add PUT and DELETE functions

--- src/routes/fruit.route.js
+++ src/routes/fruit.route.js
@@ -13,9 +13,34 @@ route.get("/fruits", async function (req, res) {
 
 route.post("/fruits", async function (req, res) {
   try {
-    const newFruit = await Fruit.create(req.body);
+    await Fruit.create(req.body);
+    let allFruits = await Fruit.find({});
 
-    res.status(200).json(newFruit);
+    res.status(200).json(allFruits);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+route.put("/fruits", async function (req, res) {
+  try {
+    const { _id, title, para, img } = req.body;
+    await Fruit.findByIdAndUpdate(_id, { title, para, img });
+
+    let result = await Fruit.find({});
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+route.delete("/fruits", async function (req, res) {
+  try {
+    const { id } = req.body;
+    await Fruit.findByIdAndDelete(id);
+
+    let result = await Fruit.find({});
+    res.status(200).json(result);
   } catch (error) {
     res.status(400).json(error.message);
   }
